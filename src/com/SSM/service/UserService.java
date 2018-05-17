c126c2b930eb20be0e57a8e5f6ba2227aa63a7f6
哒啦~哒 <37185284+LiuJiaw@users.noreply.github.com>
2018-05-17 22:23:17 +0800

    Add files via upload

--- src/com/SSM/service/UserService.java
+++ src/com/SSM/service/UserService.java
@@ -1,16 +1,17 @@
-package com.SSM.mapper;
+package com.SSM.service;
 
 import java.util.List;
 
 import com.SSM.pojo.User;
 
-public interface UserMapper {
+public interface UserService {
 	//检索全部用户
 	List<User>queryAllUsers();
 	//添加用户
 	void addUser(User user);
+	
 	//按条件检索用户
-	List<User> queryUserByItems(User user);
+	List<User>queryUserByItems(User user);
     //删除操作
 	void deletUser(User user);
 }
---
+++ src/com/SSM/service/imp/UserServiceImp.java
@@ -0,0 +1,35 @@
+package com.SSM.service.imp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.SSM.mapper.UserMapper;
+import com.SSM.pojo.User;
+import com.SSM.service.UserService;
+
+public class UserServiceImp implements UserService {
+	
+	//创建usermapper接口对象
+	@Autowired
+	private UserMapper userMapper;
+
+	public List<User> queryAllUsers() {
+		// TODO Auto-generated method stub
+		return userMapper.queryAllUsers();
+	}
+
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		userMapper.addUser(user);
+	}
+
+	public List<User> queryUserByItems(User user) {
+		// TODO Auto-generated method stub
+		return userMapper.queryUserByItems(user);
+	}
+   
+	public void deletUser(User user){
+		userMapper.deletUser(user);
+	}
+}
