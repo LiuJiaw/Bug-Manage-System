16c6390ac5fed9c87c7cdb5dbed4f1610e69342e
哒啦~哒 <37185284+LiuJiaw@users.noreply.github.com>
2018-05-19 23:30:37 +0800

    Add files via upload

---
+++ src/com/SSM/pojo/Pro_bugs.java
@@ -0,0 +1,55 @@
+package com.SSM.pojo;
+
+public class Pro_bugs {
+	private int bug_id;
+	private String pro_name;
+	private String bug_name;
+	private String bug_message;
+	private String bug_dev;
+	private String bug_user;
+	private String create_date;
+	
+	public int getBug_id() {
+		return bug_id;
+	}
+	public void setBug_id(int bugId) {
+		bug_id = bugId;
+	}
+	public String getBug_name() {
+		return bug_name;
+	}
+	public void setBug_name(String bugName) {
+		bug_name = bugName;
+	}
+	public String getPro_name() {
+		return pro_name;
+	}
+	public void setPro_name(String proName) {
+		pro_name = proName;
+	}
+	public String getBug_message() {
+		return bug_message;
+	}
+	public void setBug_message(String bugMessage) {
+		bug_message = bugMessage;
+	}
+	public String getBug_dev() {
+		return bug_dev;
+	}
+	public void setBug_dev(String bugDev) {
+		bug_dev = bugDev;
+	}
+	public String getBug_user() {
+		return bug_user;
+	}
+	public void setBug_user(String bugUser) {
+		bug_user = bugUser;
+	}
+	public String getCreate_date() {
+		return create_date;
+	}
+	public void setCreate_date(String Createdate) {
+		create_date = Createdate;
+	}
+	
+}
